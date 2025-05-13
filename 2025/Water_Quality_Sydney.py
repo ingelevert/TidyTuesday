23a62b7854f7e5081233c57bd1f5994500f1fa9b
ingelevert <linaaudia4@gmail.com>
2025-05-13 12:21:00 +0200

    Data Cleaned

--- 2025/Water_Quality_Sydney.py
+++ 2025/Water_Quality_Sydney.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-
 # Load the dataset and clean it
 Water_Quality = pd.read_csv("/Users/levilina/Documents/Coding/TidyTuesday/2025/Week_20_Water_Quality_Sydney/water_quality.csv")
 Weather = pd.read_csv("/Users/levilina/Documents/Coding/TidyTuesday/2025/Week_20_Water_Quality_Sydney/weather.csv")
@@ -30,3 +29,41 @@ print(Weather.dtypes)
 print("\nKey measurements ranges:")
 print(f"Enterococci range: {Water_Quality['enterococci_cfu_100ml'].min()} - {Water_Quality['enterococci_cfu_100ml'].max()}")
 print(f"Rainfall range: {Weather['precipitation_mm'].min()} - {Weather['precipitation_mm'].max()}")
+
+
+# Data cleaning --------------------------------------------------------------------------------------------------------------------
+
+# Clean water quality data
+def clean_water_quality(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.replace('NA', np.nan)
+    numeric_cols = ['enterococci_cfu_100ml', 'water_temperature_c', 'conductivity_ms_cm']
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    # water quality catagories, good ≤ 40 CFU/100ml, fair 41-200 CFU/100ml, poor: >200 CFU/100ml
+    bins = [-1, 40, 200, float('inf')]
+    labels = ['Good', 'Fair', 'Poor']
+    df['quality_category'] = pd.cut(df['enterococci_cfu_100ml'], bins=bins, labels=labels)
+    return df
+
+# Clean weather data
+def clean_weather(df):
+    df['date'] = pd.to_datetime(df['date'])
+    return df
+
+# Apply
+Water_Quality_clean = clean_water_quality(Water_Quality)
+Weather_clean = clean_weather(Weather)
+
+# Merge datasets
+merged_data = pd.merge(
+    Water_Quality_clean,
+    Weather_clean[['date', 'precipitation_mm', 'max_temp_C']],
+    on='date',
+    how='left'
+)
+
+# Analyze and visualize -------------------------------------------------------------------------------------------------
+
+
